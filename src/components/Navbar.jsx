bbedb216fd9a4664f2fde44064ff99edd4625113
Michael Caridad <caridadmichael@gmail.com>
2022-04-16 23:21:46 -0400

    reformat code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,117 +1,64 @@
-import {FaRegClock,FaUpload,FaRandom,FaGem} from "react-icons/fa"
-import {NavLink} from "react-router-dom"
-import styled from "styled-components"
-
+import { FaRegClock, FaUpload, FaRandom, FaGem } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 const List = styled.li`
-     display: flex;
-     justify-content: center;
-     margin: 2rem 0rem;
-     
-
+	display: flex;
+	justify-content: center;
+	margin: 2rem 0rem;
 `;
 
 const SLink = styled(NavLink)`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-right: 1rem;
-    border-radius: 3px;
-    text-decoration: none;
-    background: linear-gradient(to top,rgba(0,0,0,.5) 0,rgba(0,0,0,.1) 100%);
-    width: 7rem;
-    height: 6rem;
-    cursor: pointer;
-    transform: scale(1);
-    padding: 1em;
-    
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin-right: 1rem;
+	border-radius: 3px;
+	text-decoration: none;
+	background: linear-gradient(
+		to top,
+		rgba(0, 0, 0, 0.5) 0,
+		rgba(0, 0, 0, 0.1) 100%
+	);
+	width: 7rem;
+	height: 6rem;
+	cursor: pointer;
+	transform: scale(1);
+	padding: 1em;
 
-    h4{
-        color: white;
-        font-size: 1.3em;
-        
-        
-       
-        
-    }
+	h4 {
+		color: ${(props) => props.color};
+		font-size: 1.3em;
+	}
 
-    svg{
-        color: white;
-        font-size: 2rem;
-        
-    }
-`
-const LatestLink = styled(SLink)`
-h4{
-    color: #ad3; 
-}
-svg{
-    color: #ad3;     
-}`;
-
-const GemLink = styled(SLink)`
-h4{
-    color: #b760f0;
-}
-svg{
-    color: #b760f0;
-}`;
-
-
-
-const RandomLink = styled(SLink)`
-h4{
-    color: #e73;
-}
-svg{
-    color: #e73; 
-}`;
-
-
-const UploadLink = styled(SLink)`
-h4{
-    color: #d55;
-}
-svg{
-    color: #d55;  
-}`;
-
-
+	svg {
+		color: ${(props) => props.color};
+		font-size: 2rem;
+	}
+`;
 
 function Navbar() {
-  return (
-    <List>
-        <LatestLink to={'/latest'}>
-            <FaRegClock/>
-            <h4>Latest</h4>
-        </LatestLink>
-        <GemLink to={'/top'}>
-            <FaGem/>
-            <h4>Toplist</h4>
-        </GemLink>
-        <RandomLink to={'/random'}>
-            <FaRandom/>
-            <h4>Random</h4>
-        </RandomLink>
-        <UploadLink to={'/upload'}>
-            <FaUpload/>
-            <h4>Upload</h4>
-        </UploadLink>
-
-    </List>
-  )
-
-
-
-
-
-
+	return (
+		<List>
+			<SLink color="#ad3" to={"/latest"}>
+				<FaRegClock />
+				<h4>Latest</h4>
+			</SLink>
+			<SLink color="#b760f0" to={"/top"}>
+				<FaGem />
+				<h4>Toplist</h4>
+			</SLink>
+			<SLink color="#e73" to={"/random"}>
+				<FaRandom />
+				<h4>Random</h4>
+			</SLink>
+			<SLink color="#d55" to={"/upload"}>
+				<FaUpload />
+				<h4>Upload</h4>
+			</SLink>
+		</List>
+	);
 }
 
-
-
-
-
-
-export default Navbar
+export default Navbar;
