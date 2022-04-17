25cb9ddecdeb2f513392177a6b5b288dfc775aba
Michael Caridad <caridadmichael@gmail.com>
2022-04-16 23:44:14 -0400

    style home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,24 @@
 import Navbar from "../components/Navbar";
 import SearchBar from "../components/SearchBar";
 import styled from "styled-components";
+import primaryBackground from "../assets/blue-gradients.020-wh.jpg";
+import secondaryBackground from "../assets/bg-dark-grain.png";
 
-const container = styled.div`
-	font-size: 1.5em;
+const Container = styled.div`
+	border-top-width: 0;
+	padding: 30px 30px 5px;
 	text-align: center;
-	color: palevioletred;
+	height: 100vh;
+	background: url(${primaryBackground}) top center/cover no-repeat fixed,
+		#171717 url(${secondaryBackground}) top left repeat;
 `;
 
 function Home() {
 	return (
-		<container>
+		<Container>
 			<Navbar />
 			<SearchBar />
-		</container>
+		</Container>
 	);
 }
 
