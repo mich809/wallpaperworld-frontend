7f4fd83af29cdf6a9f10e57e07f81e9c32ccdc6f
Michael Caridad <caridadmichael@gmail.com>
2022-04-16 21:35:31 -0400

    add component to hold all routes

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
 import Home from './Home'
+import Random from './Random'
+import Latest from "./Latest"
+import Top from "./Top"
+import Upload from "./Upload"
+import {Route, Routes} from "react-router-dom"
 
 function Pages() {
-  return (
-    <div>
-        <Home/>
-    </div>
+  return (   
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/random" element={<Random/>}/>
+          <Route path="/upload" element={<Upload/>}/>
+          <Route path="/latest" element={<Latest/>}/>
+          <Route path="/top" element={<Top/>}/>
+      </Routes> 
+   
+    
   )
 }
 
