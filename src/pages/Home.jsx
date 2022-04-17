1b0bbb756f235962d12202bba4993d7df19cbae1
Michael Caridad <caridadmichael@gmail.com>
2022-04-16 23:22:09 -0400

    added navbar and search components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
+import Navbar from "../components/Navbar";
+import SearchBar from "../components/SearchBar";
+import styled from "styled-components";
+
+const container = styled.div`
+	font-size: 1.5em;
+	text-align: center;
+	color: palevioletred;
+`;
 
 function Home() {
-  return (
-      <h1>hello</h1>
-  )
+	return (
+		<container>
+			<Navbar />
+			<SearchBar />
+		</container>
+	);
 }
 
-export default Home
+export default Home;
