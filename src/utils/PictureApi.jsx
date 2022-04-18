b6541f46a67ad74b28157e2016b543ba7c73e19f
Michael Caridad <caridadmichael@gmail.com>
2022-04-18 15:48:02 -0400

    modify getRandomPictures method

--- src/utils/PictureApi.jsx
+++ src/utils/PictureApi.jsx
@@ -1,10 +1,14 @@
-import axios from 'axios'
-
+import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:8080/api/picture";
-axios.defaults.headers.common['Authorization'] = localStorage.getItem('jwt')
+axios.defaults.headers.common["Authorization"] = localStorage.getItem("jwt");
 
-
-export const getRandomPictures = async () => await axios.get('/getRandomPictures')
-                                               .then(function(response){console.log(response)})
-                                               .catch(function(error){return error})
+export const getRandomPictures = async () =>
+	await axios
+		.get("/getRandomPictures")
+		.then(function (response) {
+			return response.data;
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
