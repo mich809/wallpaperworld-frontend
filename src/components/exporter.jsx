cc1b0b08205afc56e06955cbe830ee435fd80639
Michael Caridad <caridadmichael@gmail.com>
2022-04-19 14:20:48 -0400

    import/export more stuff

--- src/components/exporter.jsx
+++ src/components/exporter.jsx
@@ -1,3 +1,6 @@
-export { default as Navbar } from "./Home/Navbar";
-export { default as HomeSearchBar } from "./Home/HomeSearchBar";
-export { default as FeatureRow } from "./Home/FeatureRow";
+export { default as Navbar } from "../pages/Home/Navbar";
+export { default as HomeSearchBar } from "../pages/Home/HomeSearchBar";
+export { default as FeatureRow } from "../pages/Home/FeatureRow";
+export { default as primaryBackground } from "../assets/blue-gradients.020-wh.jpg";
+export { default as secondaryBackground } from "../assets/bg-dark-grain.png";
+export { default as styled } from "styled-components";
