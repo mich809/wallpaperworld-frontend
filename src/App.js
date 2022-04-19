cfaffd0b0931a43fac44cf43f739b284be91da65
Michael Caridad <caridadmichael@gmail.com>
2022-04-19 14:19:49 -0400

    add all routes to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
+import Random from "./pages/Random";
+import Latest from "./pages/Latest";
+import TopList from "./pages/TopList";
+import Upload from "./pages/Upload";
+import Register from "./pages/Register";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
-		<div>
-			<BrowserRouter>
-				<Home />
-			</BrowserRouter>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/random" element={<Random />} />
+				<Route path="/upload" element={<Upload />} />
+				<Route path="/latest" element={<Latest />} />
+				<Route path="/toplist" element={<TopList />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
