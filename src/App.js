7b4af8863be7a145665a5d8f7b789ab9ecc4a622
Michael Caridad <caridadmichael@gmail.com>
2022-04-22 14:03:33 -0400

    added more routes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Latest from "./pages/Latest";
 import TopList from "./pages/TopList";
 import Upload from "./pages/Upload";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Wallpaper from "./pages/Wallpaper";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
 				<Route path="/random" element={<Random />} />
 				<Route path="/upload" element={<Upload />} />
 				<Route path="/latest" element={<Latest />} />
 				<Route path="/toplist" element={<TopList />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/wallpaper/:id" element={<Wallpaper />} />
+				<Route path="/" element={<Home />} />
 			</Routes>
 		</BrowserRouter>
 	);
