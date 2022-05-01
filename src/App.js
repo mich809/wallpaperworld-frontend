667ed42aba87fe3db075fe750eb395c544a52a06
Michael Caridad <caridadmichael@gmail.com>
2022-04-30 21:14:52 -0400

    added profile route , and protected route

--- src/App.js
+++ src/App.js
@@ -6,23 +6,29 @@ import Upload from "./pages/Upload";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Search from "./pages/Search";
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Wallpaper from "./pages/Wallpaper";
+import Profile from "./pages/Profile";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RequireAuth from "./context/RequireAuth";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/random" element={<Random />} />
-				<Route path="/upload" element={<Upload />} />
 				<Route path="/latest" element={<Latest />} />
 				<Route path="/toplist" element={<TopList />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/login" element={<Login />} />
+				<Route path="/profile" element={<Profile />} />
+
 				<Route path="/wallpaper/:id" element={<Wallpaper />} />
 				<Route path="/search/:tag" element={<Search />} />
 				<Route path="/" element={<Home />} />
+
+				<Route element={<RequireAuth />}>
+					<Route path="/upload" element={<Upload />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
