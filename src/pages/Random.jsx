e877914744a0ed13ae94f617a1a7f27efea34b69
Michael Caridad <caridadmichael@gmail.com>
2022-05-02 17:02:33 -0400

    added infinite scroll

--- src/pages/Random.jsx
+++ src/pages/Random.jsx
@@ -4,14 +4,43 @@ import { useEffect, useState } from "react";
 import { getRandomPictures } from "../utils/PictureApi";
 import Picture from "../components/Commons/Picture";
 import { FaRandom } from "react-icons/fa";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Random = () => {
 	const [pictures, setPictures] = useState([]);
+	const [pageNumber, setPageNumber] = useState(1);
+	const [hasMore, setHasMore] = useState(true);
+
 	useEffect(() => {
-		getRandomPictures().then((pics) => {
-			setPictures(pics.content);
-		});
+		getRandomPictures(0)
+			.then((response) => {
+				setPictures(response.data.content);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	}, []);
+
+	const fetchPictures = async () => {
+		return await getRandomPictures(pageNumber)
+			.then((response) => {
+				setPictures([...pictures, ...response.data.content]);
+				if (
+					response.data.content.length === 0 ||
+					response.data.content.length < 20
+				) {
+					console.log("the end");
+					setHasMore(false);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	const fetchData = async () => {
+		fetchPictures();
+		setPageNumber(pageNumber + 1);
+	};
 
 	return (
 		<>
@@ -24,16 +53,28 @@ const Random = () => {
 					</h1>
 				</header>
 				<div>
-					<section>
+					<section style={{ margin: "3em" }}>
 						<ul>
-							{pictures.map((picture, index) => (
-								<Picture
-									src={picture.pictureUrl}
-									url={picture.pictureName}
-									alt=""
-									key={index}
-								/>
-							))}
+							<InfiniteScroll
+								dataLength={pictures.length}
+								next={fetchData}
+								hasMore={hasMore}
+								loader={<h4>Loading...</h4>}
+								endMessage={
+									<p style={{ textAlign: "center" }}>
+										<b>Yay! You have seen it all</b>
+									</p>
+								}
+							>
+								{pictures.map((picture, index) => (
+									<Picture
+										src={picture.pictureUrl}
+										url={picture.pictureName}
+										alt=""
+										key={index}
+									/>
+								))}
+							</InfiniteScroll>
 						</ul>
 					</section>
 				</div>
