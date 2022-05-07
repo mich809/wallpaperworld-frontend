0f5aa6943cd679bd0f171904978dfa37864e65cb
Michael Caridad <caridadmichael@gmail.com>
2022-05-07 14:45:06 -0400

    filter out images that have not been approved

--- src/pages/Latest.jsx
+++ src/pages/Latest.jsx
@@ -4,15 +4,38 @@ import { useEffect, useState } from "react";
 import { getLatestPictures } from "../utils/PictureApi";
 import Picture from "../components/Commons/Picture";
 import { BsClockHistory } from "react-icons/bs";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Latest = () => {
 	const [pictures, setPictures] = useState([]);
+	const [pageNumber, setPageNumber] = useState(1);
+	const [hasMore, setHasMore] = useState(true);
 	useEffect(() => {
-		getLatestPictures().then((pics) => {
-			console.log(pics.content);
+		getLatestPictures(0).then((pics) => {
 			setPictures(pics.content);
 		});
 	}, []);
+
+	const fetchPictures = async () => {
+		return await getLatestPictures(pageNumber)
+			.then((response) => {
+				setPictures([...pictures, ...response.data.content]);
+				if (
+					response.data.content.length === 0 ||
+					response.data.content.length < 20
+				) {
+					console.log("the end");
+					setHasMore(false);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	const fetchData = async () => {
+		fetchPictures();
+		setPageNumber(pageNumber + 1);
+	};
 
 	return (
 		<>
@@ -21,7 +44,7 @@ const Latest = () => {
 				<header>
 					<h1>
 						<BsClockHistory
-							style={{ margin: "20 20 0 25", fontSize: "1.5em" }}
+							style={{ margin: "20 20 0 25", fontSize: "1.5em", color: "#ad3" }}
 						/>
 						Latest
 					</h1>
@@ -29,14 +52,28 @@ const Latest = () => {
 				<div>
 					<section>
 						<ul>
-							{pictures.map((picture, index) => (
-								<Picture
-									src={picture.pictureUrl}
-									url={picture.name}
-									alt=""
-									key={index}
-								/>
-							))}
+							<InfiniteScroll
+								dataLength={pictures.length}
+								next={fetchData}
+								hasMore={hasMore}
+								loader={
+									<h4 style={{ fontSize: "2em", color: "#ddd" }}>Loading...</h4>
+								}
+								endMessage={
+									<p style={{ textAlign: "center" }}>
+										<b>Yay! You have seen it all</b>
+									</p>
+								}
+							>
+								{pictures.map((picture, index) => (
+									<Picture
+										src={picture.pictureUrl}
+										url={picture.name}
+										alt=""
+										key={index}
+									/>
+								))}
+							</InfiniteScroll>
 						</ul>
 					</section>
 				</div>
